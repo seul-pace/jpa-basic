b6ea0b0dca1d21033ab33ac54469ea85a00ec87b
seulhui <lsh9412@naver.com>
2023-03-07 21:08:48 +0900

    Jpa 테스트

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,53 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        // persistence.xml 파일의 persistence-unit 이름을 괄호 안에 적는다
+        // 애플리케이션 로딩 시점에 딱 1개만
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 고객이 어떤 행위를 할 때마다 (DB 커넥션 얻어서 쿼리를 날리고 종료할 때마다) 무조건 생
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // db 트랜잭션 실행 (모든 데이터 변경은 트랜잭션 안에서 실행)
+
+        // code
+        try {
+            // 생성
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(findMember);
+
+            // 찾기
+            Member findMember = em.find(Member.class, 1L);
+
+            // JPQL
+            // JPA 가 Member 객체를 대상으로 쿼리를 짠 것이다
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            // 수정 (트랜잭션 커밋 시점에 알아채고 업데이트)
+            findMember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
